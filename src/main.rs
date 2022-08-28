dc14bb232c311dd6ede36fd04f59d63fc69d9b6f
Leonardo C <leonardorudolfocardoso@gmail.com>
2022-08-28 11:48:51 -0300

    read n number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Fibonacci generator!");
+    println!("Enter the number of fibonacci numbers you want:");
+
+    loop {
+        let mut n = String::new();
+
+        stdin().read_line(&mut n).expect("Not able to read input.");
+
+        let _n: u32 = match n.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Not able to parse \"{}\" to a number. Try again. ", n.trim());
+                continue;
+            }
+        };
+
+        break;
+    }
 }
