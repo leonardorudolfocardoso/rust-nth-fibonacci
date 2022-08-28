9086a14d1ab3e70f165a333acdf7830ba95dabc2
Leonardo C <leonardorudolfocardoso@gmail.com>
2022-08-28 12:01:36 -0300

    nth_fibonacci function

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,32 @@ use std::io::stdin;
 
 fn main() {
     println!("Fibonacci generator!");
-    println!("Enter the number of fibonacci numbers you want:");
-
     loop {
         let mut n = String::new();
 
+        println!("Enter the nth Fibonacci number you want:");
         stdin().read_line(&mut n).expect("Not able to read input.");
 
-        let _n: u32 = match n.trim().parse() {
-            Ok(num) => num,
+        let n: u32 = match n.trim().parse() {
+            Ok(num) if num > 0 => num,
+            Ok(_) => {
+                println!("Number of Fibonacci must be more than 0. Try again. ");
+                continue;
+            },
             Err(_) => {
                 println!("Not able to parse \"{}\" to a number. Try again. ", n.trim());
                 continue;
             }
         };
 
-        break;
+        println!("{n}th Fibonacci number is {}", nth_fibonacci(n));
+    }
+}
+
+fn nth_fibonacci (n: u32) -> u32 {
+    match n {
+        _ if n == 1 => 1,
+        _ if n == 2 => 1,
+        _ => nth_fibonacci(n-2) + nth_fibonacci(n-1) 
     }
 }
